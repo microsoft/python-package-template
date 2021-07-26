1380fcd58cc0ade5f66281ceb4cbb85972952088
Daniel Ciborowski <dciborow@microsoft.com>
2021-07-26 13:41:19 -0400

    Update test_methods.py

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,2 +1,18 @@
-def test_hello()
+import pytest
+
+from ai-python-package.hello_world import hello_world
+
+def hello_test()
   hello_world()
+ 
+@pytest.fixture
+def unit_test_mocks(monkeypatch):
+  """Include Mocks here to execute all commands offline and fast."""
+  pass
+
+def test_hello():
+  hello_test()  
+
+@pytest.mark.online
+def test_hello_online():
+  hello_test()
