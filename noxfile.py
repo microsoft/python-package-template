d68bf9c20e8c129d47c57909dd11ec31b3030ab3
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-15 16:52:12 -0400

    Create noxfile.py

---
+++ noxfile.py
@@ -0,0 +1,113 @@
+# Copyright 2016 Alethea Katherine Flowers
+# Source - https://github.com/wntrblm/nox/blob/main/noxfile.py
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from __future__ import annotations
+
+import functools
+import os
+import platform
+import shutil
+
+import nox
+
+ON_WINDOWS_CI = "CI" in os.environ and platform.system() == "Windows"
+
+# Skip 'conda_tests' if user doesn't have conda installed
+nox.options.sessions = ["tests", "cover", "lint", "docs"]
+if shutil.which("conda"):
+    nox.options.sessions.append("conda_tests")
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
+def tests(session):
+    """Run test suite with pytest."""
+    session.create_tmp()
+    session.install("-r", "requirements-test.txt")
+    session.install("-e", ".[tox_to_nox]")
+    tests = session.posargs or ["tests/"]
+    session.run(
+        "pytest",
+        "--cov=nox",
+        "--cov-config",
+        "pyproject.toml",
+        "--cov-report=",
+        *tests,
+        env={"COVERAGE_FILE": f".coverage.{session.python}"},
+    )
+    session.notify("cover")
+
+
+@nox.session(python=["3.7", "3.8", "3.9", "3.10"], venv_backend="conda")
+def conda_tests(session):
+    """Run test suite with pytest."""
+    session.create_tmp()
+    session.conda_install(
+        "--file", "requirements-conda-test.txt", "--channel", "conda-forge"
+    )
+    session.install("-e", ".", "--no-deps")
+    tests = session.posargs or ["tests/"]
+    session.run("pytest", *tests)
+
+
+@nox.session
+def cover(session):
+    """Coverage analysis."""
+    if ON_WINDOWS_CI:
+        return
+
+    session.install("coverage[toml]")
+    session.run("coverage", "combine")
+    session.run("coverage", "report", "--fail-under=100", "--show-missing")
+    session.run("coverage", "erase")
+
+
+@nox.session(python="3.9")
+def lint(session):
+    """Run pre-commit linting."""
+    session.install("pre-commit")
+    # See https://github.com/theacodes/nox/issues/545
+    # and https://github.com/pre-commit/pre-commit/issues/2178#issuecomment-1002163763
+    session.run(
+        "pre-commit",
+        "run",
+        "--all-files",
+        "--show-diff-on-failure",
+        "--hook-stage=manual",
+        env={"SETUPTOOLS_USE_DISTUTILS": "stdlib"},
+        *session.posargs,
+    )
+
+
+@nox.session
+def docs(session):
+    """Build the documentation."""
+    output_dir = os.path.join(session.create_tmp(), "output")
+    doctrees, html = map(
+        functools.partial(os.path.join, output_dir), ["doctrees", "html"]
+    )
+    shutil.rmtree(output_dir, ignore_errors=True)
+    session.install("-r", "requirements-test.txt")
+    session.install(".")
+    session.cd("docs")
+    sphinx_args = ["-b", "html", "-W", "-d", doctrees, ".", html]
+
+    if not session.interactive:
+        sphinx_cmd = "sphinx-build"
+    else:
+        sphinx_cmd = "sphinx-autobuild"
+        sphinx_args.insert(0, "--open-browser")
+
+    session.run(sphinx_cmd, *sphinx_args)
