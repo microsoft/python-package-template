6bf015299b9f799baef37a7c26fd4d067f1f3abd
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-23 18:04:13 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,3 +13,9 @@ def pytest_collection_modifyitems(items):
             item.add_marker(pytest.mark.spark)
         elif "_int_" in item.nodeid:
             item.add_marker(pytest.mark.integration)
+
+
+@pytest.fixture
+def unit_test_mocks(monkeypatch):
+    """Include Mocks here to execute all commands offline and fast."""
+    pass
