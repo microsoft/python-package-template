f2f092a25591b6898f095bed27ae8c1219f58199
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-22 20:25:23 -0400

    Update hello_world.py

--- src/ai_python_package/hello_world.py
+++ src/ai_python_package/hello_world.py
@@ -1,14 +1,22 @@
+#   -------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   -------------------------------------------------------------
+"""This is a Sample Python file"""
+
 def hello_world(i: int = 0) -> str:
-    "Doc String"
+    """Doc String"""
     print("hello world")
     return "string"
 
 
 def good_night() -> str:
+    """Doc String"""
     print("good night")
     return "string"
 
 
 def hello_goodbye():
+    """Doc String"""
     hello_world("x")
     good_night()
