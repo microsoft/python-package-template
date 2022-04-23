5eaab65b737dfc76ec7ec70b0d1e54d97b634ca3
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-22 20:28:02 -0400

    Update src/ai_python_package/hello_world.py
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/ai_python_package/hello_world.py
+++ src/ai_python_package/hello_world.py
@@ -4,6 +4,7 @@
 #   ---------------------------------------------------------------------------------
 """This is a Sample Python file"""
 
+
 def hello_world(i: int = 0) -> str:
     """Doc String"""
     print("hello world")
