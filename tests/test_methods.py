15cd799cdaa4b9cdd0bf609374ace0b76c08cb9c
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-23 18:03:39 -0400

    Update test_methods.py

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -3,29 +3,26 @@
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   ---------------------------------------------------------------------------------
 """This is a sample python file for testing."""
-
-import pytest
-
 from ai_python_package.hello_world import hello_world
 
 
 def hello_test():
+    """
+    This defines the expected usage, which can then be used in various test cases.
+    """
     hello_world()
 
 
-@pytest.fixture
-def unit_test_mocks(monkeypatch):
-    """Include Mocks here to execute all commands offline and fast."""
-    pass
-
-
 def test_hello(unit_test_mocks):
+    """
+    This is a simple test, which can use a mock to override online functionality.
+    """
     hello_test()
 
 
 def test_int_hello_online():
     """
-    This test is marked implicitly as an integration test because the name contains "_init"
+    This test is marked implicitly as an integration test because the name contains "_init_"
     
     https://docs.pytest.org/en/6.2.x/example/markers.html#automatically-adding-markers-based-on-test-names
     """
