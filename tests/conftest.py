6c05c96684c55689ce8e225724866b41c9b5dbf6
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-23 18:01:04 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,9 @@
+#   ---------------------------------------------------------------------------------
+#   Copyright (c) Microsoft Corporation. All rights reserved.
+#   Licensed under the MIT License. See LICENSE in project root for information.
+#   ---------------------------------------------------------------------------------
+"""This is a configuration file for pytest containing customizations and fixtures."""
+
 import pytest
 
 
