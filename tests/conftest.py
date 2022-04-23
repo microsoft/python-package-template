418f0447875fe14eb046ca8b2e00fef22aeed5fa
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-23 17:58:40 -0400

    Create conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        if "spark" in item.nodeid:
+            item.add_marker(pytest.mark.spark)
+        elif "_int_" in item.nodeid:
+            item.add_marker(pytest.mark.integration)
