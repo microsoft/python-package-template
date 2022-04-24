4c4b5de582d587760cf61aca1b4145fc45f122cd
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-23 21:29:49 -0400

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -9,7 +9,6 @@ from ai_python_package.hello_world import hello_world
 def hello_test():
     """
     This defines the expected usage, which can then be used in various test cases.
-    
     Pytest will not execute this code directly, since the function does not contain the suffex "test"
     """
     hello_world()
@@ -18,7 +17,6 @@ def hello_test():
 def test_hello(unit_test_mocks):
     """
     This is a simple test, which can use a mock to override online functionality.
-    
     unit_test_mocks: Fixture located in conftest.py, implictly imported via pytest.
     """
     hello_test()
@@ -27,7 +25,6 @@ def test_hello(unit_test_mocks):
 def test_int_hello():
     """
     This test is marked implicitly as an integration test because the name contains "_init_"
-    
     https://docs.pytest.org/en/6.2.x/example/markers.html#automatically-adding-markers-based-on-test-names
     """
     hello_test()
