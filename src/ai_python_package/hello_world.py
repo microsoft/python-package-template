7731c05c40a4360ecdce05aedfcb0a455196118f
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-25 20:24:27 +0000

    fix workspace configurations

--- src/ai_python_package/hello_world.py
+++ src/ai_python_package/hello_world.py
@@ -2,22 +2,21 @@
 #   Copyright (c) Microsoft Corporation. All rights reserved.
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   ---------------------------------------------------------------------------------
-"""This is a Sample Python file"""
+"""This is a Sample Python file."""
 
 
 def hello_world(i: int = 0) -> str:
-    """Doc String"""
+    """Doc String."""
     print("hello world")
     return f"string-{i}"
 
 
 def good_night() -> str:
-    """Doc String"""
+    """Doc String."""
     print("good night")
     return "string"
 
 
 def hello_goodbye():
-    """Doc String"""
     hello_world(1)
     good_night()
