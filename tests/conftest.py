a999e9fa94abb73f0132278c45d321e1df90f129
Daniel Ciborowski <dciborow@microsoft.com>
2022-04-26 00:24:00 +0000

    enable code coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,11 @@
 #   Copyright (c) Microsoft Corporation. All rights reserved.
 #   Licensed under the MIT License. See LICENSE in project root for information.
 #   ---------------------------------------------------------------------------------
-"""This is a configuration file for pytest containing customizations and fixtures."""
+"""
+This is a configuration file for pytest containing customizations and fixtures.
+
+In VSCode, Code Coverage is recorded in config.xml. Delete this file to reset reporting.
+"""
 
 from __future__ import annotations
 
