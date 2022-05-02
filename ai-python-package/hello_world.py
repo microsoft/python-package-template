89746252a9189b374a4cecdd82b53c57e9572f0f
Daniel Ciborowski <dciborow@microsoft.com>
2022-05-02 17:42:19 -0400

    Autoformat with Black

--- ai-python-package/hello_world.py
+++ ai-python-package/hello_world.py
@@ -2,9 +2,7 @@ def hello_world(i: int = 0) -> str:
     "Doc Stringz"
     print("hello world")
 
-    
-    
-    
+
 def good_night():
     print("good night")
     x = 2
@@ -12,9 +10,6 @@ def good_night():
     return
 
 
-
 def hello_goodbye():
     hello_world("x")
     good_night()
-
-    
