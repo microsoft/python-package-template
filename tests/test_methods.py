c312baf6e227afda8fcc2e49d45f8b4a6571e9f3
Daniel Ciborowski <dciborow@microsoft.com>
2022-05-02 20:07:40 -0400

    Update tests/test_methods.py

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -29,5 +29,4 @@ def test_int_hello():
     This test is marked implicitly as an integration test because the name contains "_init_"
     https://docs.pytest.org/en/6.2.x/example/markers.html#automatically-adding-markers-based-on-test-names
     """
-    assert 1 == 2
     hello_test()
