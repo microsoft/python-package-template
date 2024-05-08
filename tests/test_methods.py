7599c38e9b8023f5b7aaee62ada35a85de5107ed
Fakeapate <imboden.samuel@protonmail.ch>
2024-05-08 18:53:20 +0200

    fix: spelling

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -19,7 +19,7 @@ def hello_test():
 def test_hello(unit_test_mocks: None):
     """
     This is a simple test, which can use a mock to override online functionality.
-    unit_test_mocks: Fixture located in conftest.py, implictly imported via pytest.
+    unit_test_mocks: Fixture located in conftest.py, implicitly imported via pytest.
     """
     hello_test()
 
